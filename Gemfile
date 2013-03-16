73482108392d32eae81bb6596982a9a0a8d9abd8
Spring_MT <today.is.sky.blue.sky@gmail.com>
2013-03-16 14:55:44 +0900

    Add spec_helper

--- Gemfile
+++ Gemfile
@@ -1,8 +1,5 @@
 source "http://rubygems.org"
 
-# Specify your gem's dependencies in data_validator.gemspec
-gemspec
-
 group :test do
   gem 'rspec'
   gem 'rspec-core'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default, :test)
+Bundler.require(:default, :test)
+
+require 'rspec'
+
+$TESTING=true
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+
